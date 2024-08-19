32c095533a124c82d835f1c902ee807f8eeeb39d
sanjaysanju673 <sanjuvadde1111@gmail.com>
2024-08-19 21:15:57 +0530

    [Sanjay] Add:last name checking

--- UserRegistration.py
+++ UserRegistration.py
@@ -1,8 +1,8 @@
 '''
 @Author: v sanjay kumar
-@Date: 2024-08-14 11:30:30
+@Date: 2024-08-19 11:30:30
 @Last Modified by: v sanjay kumar
-@Last Modified time: 2024-08-14 11:30:30.
+@Last Modified time: 2024-08-19 11:30:30.
 @Title :Python programs of User Registration.
 
 '''
@@ -21,22 +21,41 @@ def valid_firstname(first_name):
 
     return bool-True or false of the first name
     '''
-    pattern= '(^[A-Z][a-zA-z]{2,})'
+    pattern =r'^[A-Z][a-zA-Z]{2,}$'
+
+    if re.match(pattern, first_name) :
+        return True
+    else:
+        return False
     
-    if re.search(pattern, first_name) :
+def validate_lastname(last_name):
+    ''' 
+    The function takes the last name frome the user and return the its valid or not
+
+    Parameters-str-last name of the User 
+
+    return bool-True or false 
+    '''
+    pattern =r'^[A-Z][a-zA-Z]{2,}$'
+
+    if re.match(pattern, last_name) :
         return True
     else:
         return False
 
-
 def main():
     first_name=input("Enter a first name: ")
+    last_name=input("Enter a last name: ")
 
-    if valid_firstname(first_name):
-        logger.info("first name is valid")
+    if valid_firstname(first_name) and validate_lastname(last_name):
+        logger.info("The User name is valid")
     
-    else:
+    elif not valid_firstname(first_name):
         logger.info("first name is not valid")
+    else:
+        logger.info("Last name is not valid")
+    
+    
 
 
 if __name__ == "__main__":
