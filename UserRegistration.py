b4f7a2bea5716f320a1b67ff65ced4d74d41c22d
sanjaysanju673 <sanjuvadde1111@gmail.com>
2024-08-19 17:53:30 +0530

    [Sanjay] Add:program of check the first name

---
+++ UserRegistration.py
@@ -0,0 +1,43 @@
+'''
+@Author: v sanjay kumar
+@Date: 2024-08-14 11:30:30
+@Last Modified by: v sanjay kumar
+@Last Modified time: 2024-08-14 11:30:30.
+@Title :Python programs of User Registration.
+
+'''
+
+import re
+import mylogging as log
+
+logger=log.logger_init("UserRegistration.py")
+
+
+def valid_firstname(first_name):
+    ''' 
+    The function takes the first name frome the user and return the its valid or not
+
+    Parameters-str-First name of the User 
+
+    return bool-True or false of the first name
+    '''
+    pattern= '(^[A-Z][a-zA-z]{2,})'
+    
+    if re.search(pattern, first_name) :
+        return True
+    else:
+        return False
+
+
+def main():
+    first_name=input("Enter a first name: ")
+
+    if valid_firstname(first_name):
+        logger.info("first name is valid")
+    
+    else:
+        logger.info("first name is not valid")
+
+
+if __name__ == "__main__":
+    main()
