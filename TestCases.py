a10850557d81471cfd0f89a54087f6026fe91d17
sanjaysanju673 <sanjuvadde1111@gmail.com>
2024-09-13 08:14:50 +0530

    [Sanjay] Add:test cases

--- TestCases.py
+++ TestCases.py
@@ -1,23 +1,22 @@
-'''
-@Author: v sanjay kumar
-@Date: 2024-08-14 11:30:30
-@Last Modified by: v sanjay kumar
-@Last Modified time: 2024-08-14 11:30:30.
-@Title :Python programs of User Registration test cases.
+# @Author: v sanjay kumar
+# @Date: 2024-08-14 11:30:30
+# @Last Modified by: v sanjay kumar
+# @Last Modified time: 2024-08-14 11:30:30.
+# @Title :Python programs of User Registration test cases.
 
-'''
-
-
-import unittest
+import pytest
 from UserRegistration import valid_firstname
 
-class TestNameValidation(unittest.TestCase):
-    def test_firstname(self):
-        self.assertTrue(valid_firstname("Sanjay"), "Should be valid")
-        self.assertFalse(valid_firstname("Ho"), "Should be invalid due to length")
-        self.assertFalse(valid_firstname("raju"), "Should be invalid due to lowercase first letter")
-        self.assertFalse(valid_firstname("Sanj@ay"), "Should be invalid due to presence of a special character")
-        self.assertFalse(valid_firstname("Sanjay23"), "Should be invalid due to presence of a number")
+def test_firstname():
+    assert valid_firstname("Sanjay") == True, "Should be valid"
+    assert valid_firstname("Ho") == False, "Should be invalid due to length"
+    assert valid_firstname("raju") == False, "Should be invalid due to lowercase first letter"
+    assert valid_firstname("Sanj@ay") == False, "Should be invalid due to presence of a special character"
+    assert valid_firstname("Sanjay23") == False, "Should be invalid due to presence of a number"
+
+def main():
+    test_firstname()
+    
 
 if __name__ == "__main__":
-    unittest.main()
+   main()
