5d09348df5c92e57e1ed46eb061e67e1e76b0fa1
sanjaysanju673 <sanjuvadde1111@gmail.com>
2024-09-13 10:42:43 +0530

    [Sanjay] Add:program of testing

---
+++ test_cases.py
@@ -0,0 +1,113 @@
+'''
+@Author: v sanjay kumar
+@Date: 2024-08-19 12:30:30
+@Last Modified by: v sanjay kumar
+@Last Modified time: 2024-08-19 12:30:30.
+@Title :Python programs of User Registration test cases.
+
+'''
+
+
+# test_user_registration.py
+
+import pytest
+from UserRegistration import (valid_firstname, validate_lastname, validate_email, validate_mobile_number, check_password)
+
+def test_firstname():
+    """
+    Description:
+        Tests the validation function for first names.
+
+    Parameter:
+        None
+
+    Return:
+        None
+    """
+    assert valid_firstname("Sanjay") is True, "Should be valid"
+    assert valid_firstname("Rahul") is True, "Should be valid"
+    assert valid_firstname("Ho") is False, "Should be invalid due to length"
+    assert valid_firstname("fi") is False, "Should be invalid due to length"
+    assert valid_firstname("raju") is False, "Should be invalid due to lowercase first letter"
+    assert valid_firstname("Sanj@ay") is False, "Should be invalid due to presence of a special character"
+    assert valid_firstname("Sanjay23") is False, "Should be invalid due to presence of a number"
+
+def test_valid_last_name():
+    """
+    Description:
+        Tests the validation function for last names.
+
+    Parameter:
+        None
+
+    Return:
+        None
+    """
+    assert validate_lastname("Vadde") is True, "Should be valid"
+    assert validate_lastname("Dandu") is True, "Should be valid"
+    assert validate_lastname("Mi") is False, "Should be invalid due to length"
+    assert validate_lastname("vadde") is False, "Should be invalid due to lowercase first letter"
+    assert validate_lastname("ram@s") is False, "Should be invalid due to presence of a special character"
+    assert validate_lastname("raja123") is False, "Should be invalid due to presence of a number"
+
+def test_valid_email():
+    """
+    Description:
+        Tests the validation function for emails.
+
+    Parameter:
+        None
+
+    Return:
+        None
+    """
+    assert validate_email("abc.xyz@bl.co.in") is True, "Should be valid"
+    assert validate_email("sanjay@bl.co") is True, "Should be valid"
+    assert validate_email("sanjay@blco") is False, "Should be invalid due to missing dot before TLD"
+    assert validate_email("sanjay@bl.co.in.") is False, "Should be invalid due to trailing dot"
+    assert validate_email("sanjay@gmailcom") is False, "Should be invalid due to missing dot in domain"
+    assert validate_email("@bl.co") is False, "Should be invalid due to missing user part"
+    assert validate_email("sanjay@.com") is False, "Should be invalid due to missing domain part"
+    assert validate_email("sanjay@domain.c") is False, "Should be invalid due to TLD being too short"
+    assert validate_email("sanjay@domain.toolongtld") is False, "Should be invalid due to TLD being too long"
+    assert validate_email("sanjaygamil.com") is False, "Should be invalid due to missing @ symbol"
+
+def test_valid_mobile_number():
+    """
+    Description:
+        Tests the validation function for mobile numbers.
+
+    Parameter:
+        None
+
+    Return:
+        None
+    """
+    assert validate_mobile_number("91 8431852455") is True, "Should be valid"
+    assert validate_mobile_number("91 1234567890") is True, "Should be valid"
+    assert validate_mobile_number("+44 1234567890") is True, "Should be valid"
+    assert validate_mobile_number("918431852455") is False, "Should be invalid due to missing space"
+    assert validate_mobile_number("91 84318524") is False, "Should be invalid due to insufficient digits"
+    assert validate_mobile_number("91 99198198012") is False, "Should be invalid due to excessive digits"
+    assert validate_mobile_number("91-9919453241") is False, "Should be invalid due to incorrect separator"
+    assert validate_mobile_number("9919819801") is False, "Should be invalid due to missing country code"
+    assert validate_mobile_number("91 99345688O1") is False, "Should be invalid due to non-numeric characters"
+
+def test_validate_user_password():
+    """
+    Description:
+        Tests the validation function for passwords.
+
+    Parameter:
+        None
+
+    Return:
+        None
+    """
+    assert check_password("Vadde341") is True, "Should be valid"
+    assert check_password("Dandu238") is True, "Should be valid"
+    assert check_password("Mi") is False, "Should be invalid due to length"
+    assert check_password("23") is False, "Should be invalid due to length"
+
+if __name__=="__main__":
+    pytest.main()
